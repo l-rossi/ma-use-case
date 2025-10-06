f57334dc13f282de42fc5fb760e7df2c66e1667f
Lukas Rossi <lukas.rossi@cocrafter.com>
2025-10-06 11:28:57 +0200

    Add copyright notice

--- backend/modules/regulation_fragment/infra/regulation_fragment_controller.py
+++ backend/modules/regulation_fragment/infra/regulation_fragment_controller.py
@@ -1,3 +1,21 @@
+"""
+Leveraging Legal Information Representation for Business Process Compliance
+Copyright (C) 2025 Lukas Rossi (l.rossi@tum.de)
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <https://www.gnu.org/licenses/>.
+"""
+
 from flask import Blueprint, request, jsonify, Response
 from di_container import container
 from modules.regulation_fragment.application.dto.create_regulation_fragment_dto import \
--- frontend/src/components/features/regulation-fragments/PriceModal.tsx
+++ frontend/src/components/features/regulation-fragments/PriceModal.tsx
@@ -1,3 +1,21 @@
+/*
+Leveraging Legal Information Representation for Business Process Compliance  
+Copyright (C) 2025 Lukas Rossi (l.rossi@tum.de)
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 import * as React from 'react';
 import { useQuery, useQueryClient } from '@tanstack/react-query';
 import { CircleDollarSign } from 'lucide-react';
